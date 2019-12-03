5edacd3ab8bc97b0037b6e318a6269d1e4f14d86
ryuma.inui <09feng07@gmail.com>
2019-12-03 14:05:49 +0000

    controller check

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
 
   def show
-      @tasks = Task.find(params[:id])
+      @task = Task.find(params[:id])
   end
 
   def new
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(message_params)
+      @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @tasks = Task.find(params[:id])
+      @task = Task.find(params[:id])
       @task.destroy
       
       flash[:success] = 'Task は正常に更新されました'
